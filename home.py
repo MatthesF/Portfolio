2511f1e9019b3b4fe2ea8e4ae839f62c5a555b25
photodenmark <43674729+photodenmark@users.noreply.github.com>
2022-12-21 23:03:42 +0100

    Updated homepage

--- home.py
+++ home.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.header("Hello")
+st.header("Welcome to my website! :wave:")
+
+st.markdown("Welcome to my personal website! Here you will find a variety of projects that I have worked on, all of which are written in Python. All project code can be found on my [GitHub](https://github.com/MatthesF/Portfolio) 👈")
